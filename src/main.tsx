ffaea28f7470a1d4d3f6beab64ce499be579b2d9
Katya <k.vezzadini@gmail.com>
2025-06-28 21:24:48 +0100

    Adding Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from './components/ui/provider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App />
       </QueryClientProvider>
     </Provider>
+    <Analytics />
   </React.StrictMode>,
 );
